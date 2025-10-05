e643e148dc423468504fb6ba7663474fc8dc224c
LJS0817 <57212058+LJS0817@users.noreply.github.com>
2025-10-05 13:35:56 +0900

    main

--- public/js/gameScript.js
+++ public/js/gameScript.js
@@ -3,15 +3,6 @@ const boardContainer = document.querySelector('.board');
 
 const ctx = canvas.getContext('2d');
 
-function resizeAndDraw() {
-    canvas.width = boardContainer.clientWidth;
-    canvas.height = boardContainer.clientHeight;
-}
-
-resizeAndDraw();
-
-window.addEventListener('resize', resizeAndDraw);
-
 class Vec2 {
     constructor(x, y) {
         this.x = x;
@@ -26,6 +17,19 @@ class Vec2 {
         return new Vec2(this.x - otherVec.x, this.y - otherVec.y);
     }
 }
+
+let CENTER = new Vec2(0, 0);
+
+function resizeAndDraw() {
+    canvas.width = boardContainer.clientWidth;
+    canvas.height = boardContainer.clientHeight;
+    CENTER.x = canvas.width / 2;
+    CENTER.y = canvas.height / 2;
+}
+
+resizeAndDraw();
+
+window.addEventListener('resize', resizeAndDraw);
 
 class Unit {
     constructor(position, velocity, size, color) {
@@ -56,15 +60,54 @@ class Sphere extends Unit {
 
     }
 
-    draw(ctx) {
+    draw(ctx, i) {
         ctx.beginPath();
-        // this.position, this.size, this.color 등은 모두 부모로부터 상속받은 속성입니다.
         ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
+
+
+        ctx.font = 'bold ' + (this.size - 45) + 'px "Inter"';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle = 'black';
+
+        ctx.fillText(i, this.position.x, this.position.y);
     }
 }
+
+const defaultRadius = 80;
+const padding = 110;
+const defaultColor = "#D9D9D9";
+
+let map = [
+    new Sphere(new Vec2(0, 0), defaultRadius, defaultColor), new Sphere(new Vec2(0, 0), defaultRadius, defaultColor), new Sphere(new Vec2(0, 0), defaultRadius, defaultColor),
+    new Sphere(new Vec2(0, 0), defaultRadius, defaultColor), new Sphere(new Vec2(0, 0), defaultRadius, defaultColor), new Sphere(new Vec2(0, 0), defaultRadius, defaultColor),
+    new Sphere(new Vec2(0, 0), defaultRadius, defaultColor), new Sphere(new Vec2(0, 0), defaultRadius, defaultColor), new Sphere(new Vec2(0, 0), defaultRadius, defaultColor),
+]
+
+function init() {
+    let offset = new Vec2(CENTER.x - padding - defaultRadius * 2, CENTER.y - padding - defaultRadius * 2);
+    for(let i = 0; i < map.length; i++) {
+        map[i].position.x = offset.x + (i % 3) * (defaultRadius * 2 + padding);
+        map[i].position.y = offset.y + parseInt(i / 3) * (defaultRadius * 2 + padding);
+    }
+}
+
+function update() {
+    for(let i = 0; i < map.length; i++) {
+        map[i].update();
+    }
+}
+
+function draw(c) {
+    for(let i = 0; i < map.length; i++) {
+        map[i].draw(c, i + 1);
+    }
+}
+
+init();
 
 // --- 게임 루프 ---
 function gameLoop() {
@@ -72,10 +115,10 @@ function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // 2. 유닛 상태 업데이트
-    // s.update();
+    update();
 
     // 3. 유닛 그리기
-    // s.draw(ctx);
+    draw(ctx);
 
     // 4. 다음 프레임 요청
     requestAnimationFrame(gameLoop);
